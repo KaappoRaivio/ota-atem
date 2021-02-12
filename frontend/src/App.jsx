69a260c2fdda2ff1c8dc9e0080eb820907e80499
Kaappo Raivio <kaappo.raivio@gmail.com>
2021-02-12 17:29:05 +0200

    autocommit
    Took 51 minutes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,43 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from "react";
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                Moi
-            </div>
-        );
-    }
-}
+import styles from "./App.module.css";
+import Tally from "./Tally.jsx";
+
+const useCommunication = atemIP => {
+  const [state, setState] = useState({});
+  const [connecting, setConnecting] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    console.log(atemIP);
+    const socket = new WebSocket(atemIP);
+    socket.onmessage = event => {
+      console.log(event.data);
+      if (connecting) {
+        setConnecting(false);
+      }
+      setState(JSON.parse(event.data));
+    };
+    socket.onerror = setError;
+  }, [atemIP]);
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
+  return { connecting, state, error };
+};
+
+const App = props => {
+  const index = 4;
+  const { connecting, state, error } = useCommunication("ws://192.168.10.101:7634/");
+
+  return (
+    <div className={styles.parent}>
+      <Tally state={state} index={index} />
+    </div>
+  );
+};
 
 App.propTypes = {};
 
---
+++ frontend/src/Tally.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Tally.module.css";
+
+const Tally = ({ state, index }) => {
+  let color = "black";
+  if (state?.program?.index === index) {
+    color = "#ff0000";
+  } else if (state?.preview?.index === index) {
+    color = "#00ff00";
+  }
+
+  return <div style={{ backgroundColor: color }} className={styles.parent} />;
+};
+
+Tally.propTypes = {};
+
+export default Tally;
