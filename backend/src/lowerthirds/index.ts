b948492f98419269536ae25b8eebd0173045d290
jusola <juulain@gmail.com>
2021-02-14 22:55:28 +0200

    Change html load filename

--- backend/src/lowerthirds/index.ts
+++ backend/src/lowerthirds/index.ts
@@ -2,7 +2,7 @@ import { LowerThirdsOptions } from "../types/lowerThirdsOptions";
 import nodeHtmlToImage from "node-html-to-image";
 import fs from "fs";
 
-const template = fs.readFileSync("./template.html", {
+const template = fs.readFileSync("./lowerthirds.html.template", {
     encoding: "utf-8",
 });
 async function render(lowerThirdsOptions: LowerThirdsOptions) {
