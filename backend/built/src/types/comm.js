db1023e954016c962f6910c52e04aeff0449acff
Kaappo Raivio <kaappo.raivio@gmail.com>
2021-02-24 00:37:44 +0200

    niin ja olishan kiva jos asioita olis gitignoressa .)))))))

--- backend/built/src/types/comm.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=comm.js.map
--- backend/built/src/validators.js
+++
@@ -1,7 +0,0 @@
-import Ajv from "ajv";
-import mediaControlRequestSchema from "./schemas/mediaControlRequest.json";
-import mediaPreparationRequestSchema from "./schemas/mediaPreparationRequest.json";
-const ajv = new Ajv();
-export const validateMediaControlRequest = ajv.compile(mediaControlRequestSchema);
-export const validateMediaPreparationRequest = ajv.compile(mediaPreparationRequestSchema);
-//# sourceMappingURL=validators.js.map
