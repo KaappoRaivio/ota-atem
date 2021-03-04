2d19466dcbfb5672299de75641cb209ea769bea4
Kaappo Raivio <kaappo.raivio@gmail.com>
2021-03-04 09:16:04 +0200

    hotfix5
    Took 3 minutes

--- frontend/src/LowerThirds.jsx
+++ frontend/src/LowerThirds.jsx
@@ -7,6 +7,7 @@ import Media from "./Media";
 import { useCommunication } from "./TallyLanding";
 
 const LowerThirds = ({ serverAddress }) => {
+    const [serverAddress, setServerAddress] = useState(params.get("serverAddress") || window.location.hostname);
     const { connected, state, error } = useCommunication(serverAddress, json => json.type === "media");
     return <Media state={state} serverAddress={serverAddress} />;
 };
