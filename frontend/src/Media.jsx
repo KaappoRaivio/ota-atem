433e2ed689ab33419756fdd4e76eede2434b1b25
Kaappo Raivio <kaappo.raivio@gmail.com>
2021-05-12 16:10:50 +0300

    autocommit
    Took 18 minutes

--- frontend/src/Media.jsx
+++ frontend/src/Media.jsx
@@ -12,32 +12,60 @@ const sendIndex = (index, serverAddress) => {
     });
 };
 
-const updateLowerThirds = (index, newLowerThirds, serverAddress) => {
+const updateLowerThirds = (index, newLowerThirds, serverAddress, action) => {
     console.log(`http://${serverAddress}/updateLowerThirds`);
-    fetch(`http://${serverAddress}/updateLowerThirds`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ action: "set", index, item: JSON.parse(newLowerThirds) }),
-    }).then(console.log);
+    try {
+        return fetch(`http://${serverAddress}/updateLowerThirds`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ action, index, item: JSON.parse(newLowerThirds) }),
+        })
+            .then(res => res.ok)
+            .catch(err => false);
+    } catch {
+        return new Promise(resolve => resolve(false));
+    }
 };
 
 const Media = ({ state, serverAddress }) => {
     console.log(state, serverAddress);
 
     const [currentJSON, setCurrentJSON] = useState("");
+
+    const [submitResult, setSubmitResult] = useState(null);
+    useEffect(() => {
+        console.log(submitResult);
+        if (submitResult != null) {
+            setTimeout(() => {
+                setSubmitResult(null);
+            }, 5000);
+        }
+    }, [submitResult]);
+
     useEffect(() => {
         setCurrentJSON(JSON.stringify(state.currentValues, null, 2));
     }, [state.currentValues]);
 
+    const resetCurrentValue = () => {
+        setCurrentJSON(JSON.stringify(state.currentValues, null, 2));
+    };
+
     return (
         <div className={`${styles.parent}`}>
-            <div className={styles.child}>
-                <p className={styles.index}>{state.currentIndex}</p>
+            <div className={styles.index}>{state.currentIndex}</div>
 
-                {/*<p className={styles.subtitle}>{state.currentValues?.subtitle}</p>*/}
+            <div className={styles.input}>
+                <textarea
+                    className={styles.textarea}
+                    name={"update lowerthirds"}
+                    rows={"200"}
+                    value={currentJSON}
+                    onChange={e => setCurrentJSON(e.target.value)}
+                />
             </div>
+
             <div className={styles.buttons}>
                 <button
                     className={styles.button}
@@ -50,19 +78,21 @@ const Media = ({ state, serverAddress }) => {
                 <button className={styles.button} onClick={() => sendIndex(state.currentIndex - 1, serverAddress)}>
                     Prev
                 </button>
-                <button className={styles.button} onClick={() => updateLowerThirds(state.currentIndex, currentJSON, serverAddress)}>
-                    Submit
+                <button
+                    style={{ backgroundColor: submitResult != null ? (submitResult ? "green" : "red") : "white" }}
+                    className={styles.button}
+                    onClick={() => updateLowerThirds(state.currentIndex, currentJSON, serverAddress, "set").then(ok => setSubmitResult(ok))}>
+                    Change
                 </button>
-            </div>
-
-            <div className={styles.input}>
-                <textarea
-                    className={styles.textarea}
-                    name={"update lowerthirds"}
-                    rows={"20"}
-                    value={currentJSON}
-                    onChange={e => setCurrentJSON(e.target.value)}
-                />
+                <button
+                    style={{ backgroundColor: submitResult != null ? (submitResult ? "green" : "red") : "white" }}
+                    className={styles.button}
+                    onClick={() => updateLowerThirds(state.currentIndex, currentJSON, serverAddress, "add").then(ok => setSubmitResult(ok))}>
+                    Add
+                </button>
+                <button className={styles.button} onClick={() => resetCurrentValue()}>
+                    Reset
+                </button>
             </div>
         </div>
     );
